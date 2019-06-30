2707d7910da657ea3849ef2683b05d4e44cb4439
MiguelichJr <owenmejia@upeu.edu.pe>
2019-06-30 15:15:20 -0500

    sjsjsjs

--- app/Http/Controllers/VentasController.php
+++ app/Http/Controllers/VentasController.php
@@ -30,7 +30,7 @@ class VentasController extends Controller
 
     public function crearVenta(Request $request) {
 
-        $idventas = DB::table('ventas')->insertGetId([
+        $idventas = DB::table('ventas')->insert([
             "idventas"=>null,
             "fecha"=>Carbon::now()->toDateString(),
             "idpersona"=>$request->idcliente,
